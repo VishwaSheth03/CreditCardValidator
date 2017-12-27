14a399d96abfb7cd610e1f6be30bd0e14374eb61
Vishwa Sheth <vishwasheth03@yahoo.com>
2017-12-27 12:22:00 -0500

    Add files via upload

---
+++ Credit Card Validator/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.creditCardNumInput = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.creditCardNumInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(61, 40);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(162, 77);
+            this.checkButton.TabIndex = 0;
+            this.checkButton.Text = "Check validity";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please enter credit card number";
+            // 
+            // creditCardNumInput
+            // 
+            this.creditCardNumInput.Location = new System.Drawing.Point(84, 12);
+            this.creditCardNumInput.Maximum = new decimal(new int[] {
+            -1304428545,
+            434162106,
+            542,
+            0});
+            this.creditCardNumInput.Minimum = new decimal(new int[] {
+            1316134912,
+            2328,
+            0,
+            0});
+            this.creditCardNumInput.Name = "creditCardNumInput";
+            this.creditCardNumInput.Size = new System.Drawing.Size(120, 22);
+            this.creditCardNumInput.TabIndex = 3;
+            this.creditCardNumInput.Value = new decimal(new int[] {
+            1316134912,
+            2328,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(295, 176);
+            this.Controls.Add(this.creditCardNumInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkButton);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.creditCardNumInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown creditCardNumInput;
+    }
+}
+
---
+++ Credit Card Validator/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Procedure to return the sum of the even and odd numbers of the reversed card number
+        // as per the necessary rules
+        private int reversedNumSum(string num)
+        {
+            string reverseCardNum = "";
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = num.Length - 1; i >= 0; i = i-1)
+            {
+                reverseCardNum += num.Substring(i,1);
+            }
+
+            for (int x = 1; x <= reverseCardNum.Length; x = x + 2)
+            {
+                oddSum = oddSum + Convert.ToInt32(reverseCardNum.Substring(x - 1, 1));
+            }
+
+            for (int x = 2; x <= reverseCardNum.Length; x = x + 2)
+            {
+                string y;
+                y = Convert.ToString(Convert.ToInt32(reverseCardNum.Substring(x - 1, 1)) * 2);
+
+                if (y.Length == 1)
+                {
+                    evenSum = evenSum + Convert.ToInt32(y);
+                }
+                else if (y.Length == 2)
+                {
+                    evenSum = evenSum + Convert.ToInt32(y.Substring(0, 1))
+                        + Convert.ToInt32(y.Substring(1, 1));
+                }
+
+            }
+
+            return (oddSum + evenSum);
+        }
+        
+        // Procedure to return card type using the card number
+        private string creditCardType(string cardNum)
+        {
+            string cardType = "";
+
+            if (cardNum.Substring(0, 1) == "4"
+                && cardNum.Length == 16
+                && reversedNumSum(cardNum) % 10 == 0)
+            {
+                cardType = "VISA card is";
+            }
+
+            else if (Convert.ToInt32(cardNum.Substring(0, 2)) >= 51
+                && Convert.ToInt32(cardNum.Substring(0, 2)) <= 55
+                && cardNum.Length == 16
+                && reversedNumSum(cardNum) % 10 == 0)
+            {
+                cardType = "Master Card (Diners Club for you Americans) is";
+            }
+
+            else if (cardNum.Substring(0, 2) == "34"
+                || cardNum.Substring(0, 2) == "37"
+                && cardNum.Length == 15
+                && reversedNumSum(cardNum) % 10 == 0)
+            {
+                cardType = "American Express is";
+            }
+
+            else if (cardNum.Substring(0, 4) == "6011" 
+                    && cardNum.Length == 16
+                    && reversedNumSum(cardNum) % 10 == 0)
+            {
+                cardType = "Discover is";
+            }
+
+            else if (cardNum.Substring(0, 2) == "36" 
+                    && cardNum.Length == 14
+                    && reversedNumSum(cardNum) % 10 == 0)
+            {
+                cardType = "Diners Club (outside U.S. and Canada) is";
+            }
+
+            else
+            {
+                cardType = "is not";
+            }
+
+            return cardType;
+
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            {
+                string cardNum = creditCardNumInput.Text;
+
+                if (cardNum != "")
+                {
+                    label1.Text = "This " + creditCardType(cardNum) + " valid";
+                }
+                else
+                    label1.Text = "Please enter credit card number";
+            }
+        }
+    }
+}
